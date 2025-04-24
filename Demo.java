eade4a52f814bfd619ff6eb781bdec17180d1568
Pajju-k <imprajwalsk@gmail.com>
2025-04-24 11:08:01 +0530

    save a file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+
+    static int a = 100;
+    static int b = 50;
+
+    public static void main(String[] args) {
+        System.out.println("Addition is " + (a + b));
+        System.out.println("Addition is " + (a - b));
+    }
+}
