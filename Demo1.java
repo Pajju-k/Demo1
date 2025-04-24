189170dfbb5bd4f89a45101cc0535a309fab43fa
Pajju-k <imprajwalsk@gmail.com>
2025-04-24 11:15:34 +0530

    save a file

---
+++ Demo1.java
@@ -0,0 +1,12 @@
+
+public class Demo1 {
+
+        static int a = 100;
+        static int b = 50;
+    
+        public static void main(String[] args) {
+            System.out.println("Subtraction is " + (a * b));
+            System.out.println("Division is " + (a / b));
+        }
+    }
+    
